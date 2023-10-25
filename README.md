d80d023f51be44918ce824044409abc0ee9cded1
Ebrahim Aboulfadl <ebrahimaboulfadl@gmail.com>
2023-10-25 14:55:19 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# QUIZ APP USING REACT
+
+this app is developed to practice :
+
+- using React Hooks
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Header from "./components/Header";
+
+
+function App() {return <Header></Header>}
+
+export default App;
