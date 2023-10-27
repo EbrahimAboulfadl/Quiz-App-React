24ae8b63593a953b8c3b98cb336db3ed3071e2bf
Ebrahim Aboulfadl <ebrahimaboulfadl@gmail.com>
2023-10-27 20:09:42 +0200

    day three

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,4 +1,7 @@
-export default function QuestionTimer() {
-    
+import { useState } from "react";
+export default function QuestionTimer({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+  setTimeout(onTimeout, timeout);
+
   return <progress id="question-time"></progress>;
 }
